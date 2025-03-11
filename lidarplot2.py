5841713aa6dbe7d72ee0965d1d26a3ecff7c194c
henry321yu <henry321yu@gmail.com>
2025-03-11 10:39:07 +0800

    dddd

--- lidarplot2.py
+++ lidarplot2.py
@@ -26,20 +26,23 @@ print(f"開始收集 LiDAR 數據，每 {max_points} 筆重繪一次...")
 
 data_list = []  # 存儲數據的列表
 
+count = 0
+start_time = time.perf_counter()
+
 while True:
     try:
         raw_data = ser.read(10000).decode(errors='ignore')  # 讀取數據
-        parts = raw_data.split("\t9999\t")  # 以 9999 作為換行分隔
+        parts = raw_data.split("\t9999\t")  # 以 9999 作為換行分隔        
 
         for part in parts:
             fields = part.strip().split("\t")  # 根據制表符分割數據
-            if len(fields) != 6:
-                continue  # 確保有 6 個欄位
+            if len(fields) != 7:
+                continue  # 確保欄位數
 
             try:
-                timee, angle, distance = map(float, fields[:3])  # 只取前三個欄位
+                timee, angle, distance, accx, accy, accz, temp = map(float, fields)  # 只取前三個欄位
             except ValueError:
-                continue  # 如果轉換失敗，跳過這筆數據
+                continue  # 如果轉換失敗，跳過這筆數據            
 
             # 轉換為 x, y 座標
             angle_rad = np.radians(90 - angle)
@@ -47,21 +50,26 @@ while True:
             y = distance * np.sin(angle_rad)
 
             data_list.append((x, y))  # 加入新數據
+            count +=1
 
             # 當數據達到 max_points 時，更新繪圖並清空舊數據
             if len(data_list) >= max_points:
+                print(fields[0:7])
+                
+                elapsed = time.perf_counter() - start_time
+                f=count / elapsed
+                count = 0
+                start_time = time.perf_counter()
+
                 current_time = time.strftime("%H:%M:%S") + f".{(time.time() % 1):.2f}"[2:]  # 取得當前時間
 
                 x_vals, y_vals = zip(*data_list)  # 轉換為 x, y 陣列
                 sc.set_offsets(np.c_[x_vals, y_vals])  # 更新點座標
                 plt.draw()
-                plt.pause(0.01)  # 避免 CPU 過載
-
-                # 輸出到終端
-                print(f"[{current_time}] 已收集 {max_points} 筆數據，清除舊數據並繪製新數據...")
+                plt.pause(0.001)  # 避免 CPU 過載
 
                 # 更新標題
-                ax.set_title(f"Real-time LiDAR Data - Last {max_points} Points\n[{current_time}]")
+                ax.set_title(f"Real-time LiDAR Data - Last {max_points} Points\n[{current_time}] - {f:.2f} Hz")
 
                 data_list.clear()  # 清空舊數據，開始收集下一批
 
