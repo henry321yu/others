fff95f1f0a40ef4b6e7ab8bf5aa90100892fce3d
henry321yu <henry321yu@gmail.com>
2025-06-24 02:00:18 +0800

    update

--- wire_voltage_calculater.py
+++ wire_voltage_calculater.py
@@ -4,14 +4,14 @@ import math
 # resistivity = 1.68e-8 #wiki copper
 resistivity = 1.8065e-8
 
-# 銅線直徑（mm）與長度（m）
+# 導線直徑（mm）與長度（m）
 diameter_mm = 0.5
 length_m = 1
 
 # 電源與負載
 # voltage = 110
-# voltage = 5
-voltage = 12 #voltage
+voltage = 5
+# voltage = 12 #voltage
 load_power = 15 #walt
 
 # 換算成米並計算橫截面面積 A = πr^2
@@ -20,16 +20,18 @@ area_mm2 = math.pi * (diameter_mm / 2) ** 2
 # 每公尺的電阻
 resistance_per_meter = resistivity / area_mm2 * 1e6
 
-# 計算總電阻
+# 計算導線總電阻
 wire_resistance = resistance_per_meter * length_m
-
+# 理論負載電阻
 load_resistence = voltage ** 2 / load_power
-
+# 總電流
 total_current = voltage / ( load_resistence + wire_resistance )
-
+# 導線壓降
 wire_voltdrop = total_current * wire_resistance
+# 負載壓降
 load_voltdrop = voltage - wire_voltdrop
-load_persent = load_voltdrop / voltage * 100
+# 損失占比
+lost_persent = (voltage - load_voltdrop) / voltage * 100
 
 
 
@@ -43,4 +45,5 @@ print(f"電源電壓: {voltage} v")
 print(f"負載功率: {load_power} w")
 print(f"導線電阻: {wire_resistance*1e3:.3f} mohm")
 print(f"導線壓降: {wire_voltdrop:.3f} v")
-print(f"負載電壓: {load_voltdrop:.3f} v ({load_persent:.2f}%)")
+print(f"負載電壓: {load_voltdrop:.3f} v")
+print(f"損失占比: {lost_persent:.2f} %")
