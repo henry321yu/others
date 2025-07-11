c767a47e5c4795549457be766e135856d6756f9e
hey <46470927+henry321yu@users.noreply.github.com>
2025-07-11 17:26:44 +0800

    Update read acc.py

--- read acc.py
+++ read acc.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 from datetime import datetime
 
 # 讀取資料
-file_path = r"C:\Users\sgrc - 325\Desktop\seismograph data\perm_0707_79.txt"
-df = pd.read_csv(file_path, header=None, names=["time", "ax", "ay", "az", "magnitude"])
+file_path = r"C:\Users\sgrc - 325\Desktop\seismograph data\perm_0711_91.txt"
+df = pd.read_csv(file_path, header=None, names=["timee","time", "ax", "ay", "az", "magnitude", "events", "status", "f", "file"])
 markersizee = 0.1
 
 # 將時間欄位轉為 datetime 格式
@@ -12,12 +12,12 @@ df["time"] = pd.to_datetime(df["time"], format="%Y-%m-%d %H:%M:%S.%f")
 
 # 繪圖
 plt.figure(figsize=(12, 6))
-plt.plot(df["time"], df["ax"], label="ax", marker='.', markersize = markersizee)
-plt.plot(df["time"], df["ay"], label="ay", marker='.', markersize = markersizee)
-plt.plot(df["time"], df["az"], label="az", marker='.', markersize = markersizee)
-plt.plot(df["time"], df["magnitude"], label="magnitude", marker='.', markersize = markersizee)
+plt.plot(df["timee"], df["ax"], label="ax", marker='.', lw = 1, markersize = markersizee)
+plt.plot(df["timee"], df["ay"], label="ay", marker='.', lw = 1, markersize = markersizee)
+plt.plot(df["timee"], df["az"], label="az", marker='.', lw = 1, markersize = markersizee)
+plt.plot(df["timee"], df["magnitude"], label="magnitude", lw = 1, marker='.', markersize = markersizee)
 
-plt.title("Sensor Data Over Time")
+plt.title(f"{file_path}")
 plt.xlabel("Time")
 plt.ylabel("Values")
 plt.grid(True)
