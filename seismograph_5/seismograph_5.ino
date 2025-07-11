937948312e09497cef63344bdad3b088fa9f2c84
hey <46470927+henry321yu@users.noreply.github.com>
2025-07-11 15:35:14 +0800

    Update seismograph_5.ino

--- seismograph_5/seismograph_5.ino
+++ seismograph_5/seismograph_5.ino
@@ -7,7 +7,7 @@
 #include <SoftwareSerial.h> //HC12(mcu's rx、hc-12's tx,   mcu's tx、hc-12's rx) 
 #include "register.h"
 
-#define SAMPLE_HZ 400
+#define SAMPLE_HZ 100
 #define SAMPLE_INTERVAL_MS (1000 / SAMPLE_HZ)
 #define THRESHOLD_G 0.03
 #define SD_CS BUILTIN_SDCARD
@@ -61,7 +61,7 @@ void setup() {
   
   Serial.println("Initializing...");
 
-  Serial.printf("Watchdog timeout : %d s\n",wdtt/1000);
+  Serial.printf("Watchdog timeout :%ds\n",wdtt/1000);
   
   setSyncProvider(getTeensy3Time);
   if (timeStatus() != timeSet) {
@@ -141,8 +141,8 @@ void loop() {
 
     acc_data();
     float magnitude = sqrt((ax - int_ax) * (ax - int_ax) + (ay - int_ay) * (ay - int_ay) + (az - int_az) * (az - int_az));
-    String acc_String_dataString = (ax, 5) + "," + String(ay, 5) + "," + String(az, 5);
-    String data = timeStamp() + "," + acc_String_dataString + "," + String(magnitude, 5) + "," + String(events) + "," + statuss + "," + String(freq, 2) + "," + nowfile;
+    String acc_String_data = String(ax, 5) + "," + String(ay, 5) + "," + String(az, 5) + "," + String(magnitude, 5);
+    String data = timeStamp() + "," + acc_String_data + "," + String(events) + "," + statuss + "," + String(freq, 2) + "," + nowfile;
 
     Serial.println(data);
     HC12.println(data);
