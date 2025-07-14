ccaa43b48cb7ce3ca9ef6f6f2004fece10939abe
hey <46470927+henry321yu@users.noreply.github.com>
2025-07-14 16:32:24 +0800

    Update hc12readsei_3.py

--- hc12readsei_3.py
+++ hc12readsei_3.py
@@ -29,18 +29,18 @@ fig, axs = plt.subplots(
     4, 1,
     figsize=(8, 6),
     sharex=True,
-    gridspec_kw={'height_ratios': [2.5, 1, 1, 1]}  # s_data 高度為其他的 2 倍
+    gridspec_kw={'height_ratios': [1, 1, 1, 2.5]}  # s_data 高度為其他的 2 倍
 )
-(line_s,) = axs[0].plot([], [], label="bia", lw=1)
-(line_x,) = axs[1].plot([], [], label="ax", color='orange', lw=1)
-(line_y,) = axs[2].plot([], [], label="ay", color='green', lw=1)
-(line_z,) = axs[3].plot([], [], label="az", color='red', lw=1)
+(line_x,) = axs[0].plot([], [], label="ax", lw=1)
+(line_y,) = axs[1].plot([], [], label="ay", color='orange', lw=1)
+(line_z,) = axs[2].plot([], [], label="az", color='green', lw=1)
+(line_s,) = axs[3].plot([], [], label="bia", color='red', lw=1)
 
 # 設定子圖標題與 Y 標籤
-axs[0].set_ylabel("bia (g)")
-axs[1].set_ylabel("ax (g)")
-axs[2].set_ylabel("ay (g)")
-axs[3].set_ylabel("az (g)")
+axs[0].set_ylabel("ax (g)")
+axs[1].set_ylabel("ay (g)")
+axs[2].set_ylabel("az (g)")
+axs[3].set_ylabel("bia (g)")
 axs[3].set_xlabel("Program time (s)")
 
 for ax in axs:
@@ -52,16 +52,16 @@ def update_plot(frame):
     """更新繪圖資料"""
     if timee_data and s_data:
         # 更新每條線的資料
-        line_s.set_data(timee_data, s_data)
         line_x.set_data(timee_data, x_data)
         line_y.set_data(timee_data, y_data)
         line_z.set_data(timee_data, z_data)
+        line_s.set_data(timee_data, s_data)
 
         # 動態調整 Y 軸範圍
-        axs[0].set_ylim(min(s_data) - k, max(s_data) + k)
-        axs[1].set_ylim(min(x_data) - k, max(x_data) + k)
-        axs[2].set_ylim(min(y_data) - k, max(y_data) + k)
-        axs[3].set_ylim(min(z_data) - k, max(z_data) + k)
+        axs[0].set_ylim(min(x_data) - k, max(x_data) + k)
+        axs[1].set_ylim(min(y_data) - k, max(y_data) + k)
+        axs[2].set_ylim(min(z_data) - k, max(z_data) + k)
+        axs[3].set_ylim(min(s_data) - k, max(s_data) + k)
 
         # 調整 X 軸範圍
         axs[3].set_xlim(min(timee_data), max(timee_data))
@@ -69,10 +69,10 @@ def update_plot(frame):
         # 更新標題
         axs[0].set_title(f"time: {timee_data[-1]} , {time_data[-1]} , freq: {f} , {atemp}°C\nevents: {events} , status: {status} , last event: {last_event_time}\n{file}" if last_event_time else f"time: {timee_data[-1]} , {time_data[-1]} , freq: {f} , {atemp}°C\nevents: {events} , status: {status}\n{file}")
 
-        axs[0].set_xlabel(f"bia: {s_data[-1]:.5f}")
-        axs[1].set_xlabel(f"ax: {x_data[-1]:.5f}")
-        axs[2].set_xlabel(f"ay: {y_data[-1]:.5f}")
-        axs[3].set_xlabel(f"az: {z_data[-1]:.5f}")
+        axs[0].set_xlabel(f"ax: {x_data[-1]:.5f}")
+        axs[1].set_xlabel(f"ay: {y_data[-1]:.5f}")
+        axs[2].set_xlabel(f"az: {z_data[-1]:.5f}")
+        axs[3].set_xlabel(f"bia: {s_data[-1]:.5f}")
 
     return line_s, line_x, line_y, line_z
 
