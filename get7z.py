93ec47b16f20cd3c471445925451c5c21641d4e5
hey <46470927+henry321yu@users.noreply.github.com>
2025-08-25 15:12:42 +0800

    Update get7z.py

--- get7z.py
+++ get7z.py
@@ -5,15 +5,15 @@ from datetime import datetime
 # 取得今天日期，格式：日月年，例如 250825
 today_str = datetime.now().strftime("%d%m%y")
 
-# # 壓縮來源資料夾
-# source_folder = Path(r"D:\fly_data")
-# # 輸出壓縮檔路徑
-# output_file = Path(fr"D:\fly_data_{today_str}.7z")
+# 壓縮來源資料夾
+source_folder = Path(r"D:\fly_data")
+# 輸出壓縮檔路徑
+output_file = Path(fr"D:\fly_data_{today_str}.7z")
 
 # 壓縮來源資料夾
-source_folder = Path(r"C:\Users\sgrc - 325\Desktop\seismograph data\old")
-# 輸出壓縮檔路徑
-output_file = Path(fr"C:\Users\sgrc - 325\Desktop\seismograph data\old_{today_str}.7z")
+# source_folder = Path(r"C:\Users\sgrc - 325\Desktop\seismograph data\old")
+# # 輸出壓縮檔路徑
+# output_file = Path(fr"C:\Users\sgrc - 325\Desktop\seismograph data\old_{today_str}.7z")
 
 # 如果檔案已存在，先刪除
 if output_file.exists():
@@ -33,9 +33,11 @@ cmd = [
     "-t7z",
     "-mx=7",
     "-m0=LZMA2",
-    "-md=1024m",
     "-mfb=273",
-    "-mmt=10"
+    # "-md=1024m",
+    # "-mmt=10"
+    "-md=512m",
+    "-mmt=18"
 ]
 
 # 執行命令
