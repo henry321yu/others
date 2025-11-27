dbb3bf96f1a8a4abf764c0d6359ddded00d61eb6
hey <46470927+henry321yu@users.noreply.github.com>
2025-11-27 15:39:35 +0800

    Update ssh_script.bat

--- sar/ssh_script.bat
+++ sar/ssh_script.bat
@@ -3,7 +3,7 @@ chcp 65001 >nul
 
 echo 正在執行 script.txt 內容...
 
-type script.txt | plink -ssh -pw eos31470. sgrc-325@10.241.0.114
+type script.txt | plink -batch -ssh -pw eos31470. sgrc-325@10.241.0.114
 
 echo.
 echo 執行完畢
