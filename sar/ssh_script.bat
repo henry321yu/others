c0e65249b2e35dc65565f40b22207f52e4b85d67
hey <46470927+henry321yu@users.noreply.github.com>
2025-12-01 14:23:37 +0800

    workkkk!

--- sar/ssh_script.bat
+++ sar/ssh_script.bat
@@ -3,7 +3,8 @@ chcp 65001 >nul
 
 echo running script.txt ...
 
-type script.txt | plink -batch -ssh -pw eos31470. sgrc-325@10.241.0.114
+% type script.txt | plink -ssh -pw 123 remote@192.168.137.30
+type script.txt | plink -ssh -pw 123 remote@192.168.50.146
 
 echo.
 echo script finished
